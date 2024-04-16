4ed3fe024060e7a7fa4c0fe10f466c4e8d65f177
mateussspinheiro <pinheiromateus900@gmail.com>
2024-04-16 19:26:32 -0300

    Estudando python

---
+++ exercicios/tiposdevariaves.py
@@ -0,0 +1,12 @@
+#como atribuir tipos as variaves:
+
+a = int(1);#inteiro
+b = float(1.000);#real
+c = str("test");#string = char
+
+#Não precisamos definir o tipo da variavel, só precisamos atribuir o valor;
+
+a = "test";
+a = 1.000;
+
+print(a);
